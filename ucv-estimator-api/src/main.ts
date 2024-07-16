5faea361a4a75e0b3e6f569d9ed3479d592d8921
jhayesnv <jhayesdev@icloud.com>
2024-07-15 21:54:18 -0700

    Implement basic logic for authenticating users

--- ucv-estimator-api/src/main.ts
+++ ucv-estimator-api/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['asd;lk1j3;4lj0ads9f09'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
---
+++ ucv-estimator-api/src/users/auth.service.ts
@@ -0,0 +1,50 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+// avoid using callbacks in base scrypt
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // see if email is already in use
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email already in use');
+    }
+    // hash the users password
+    // generate salt
+    const salt = randomBytes(8).toString('hex');
+    // hash salt and password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    // join the hashed result and the salt together
+    const result = salt + '.' + hash.toString('hex');
+    // create a new user and save it
+    const user = await this.usersService.create(email, result);
+    // return the user
+    return user;
+  }
+
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    // separate salt and hash from password
+    const [salt, storedHash] = user.password.split('.');
+    // compare stored hash from user provided hash
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Bad password');
+    }
+    return user;
+  }
+}
